799ee0d19314bb29c2478ae599b725797775e448
David Workman <workmad3@gmail.com>
2014-04-29 14:48:55 +0100

    Improving setup of lograge during production

--- lib/lograge-params/engine.rb
+++ lib/lograge-params/engine.rb
@@ -1,43 +1,45 @@
-ENV['USE_LOGRAGE'] ||= 'true' if Rails.env.production?
+require 'useragent'
 
 module LogrageParams
   class Engine < ::Rails::Engine
-    if ENV['USE_LOGRAGE'] == 'true'
-      require 'useragent'
-      config.lograge.enabled = true
-
-      config.lograge.custom_options = lambda do |event|
-        unwanted_keys = %w[format action controller
-                           utf8 authenticity_token commit]
-        flattener = proc do |(k, v), h, prefix='params'|
-          key = [prefix, k].compact.join("/")
-          if %w(password password_confirmation).include? k
-            h[key] = "[FILTERED]"
-          elsif v.is_a?(Hash)
-            v.each {|p| flattener.call(p, h, key)}
-          else
-            h[key] = "'#{v}'"
-          end
-        end
-
-        user_agent = UserAgent.parse(event.payload[:browser])
-        browser_log = {
-          browser: user_agent.browser,
-          platform: user_agent.platform,
-          browser_version: user_agent.version
-        }
-
-        params = event.payload[:params].reject { |key,_| unwanted_keys.include? key }.each_with_object({}, &flattener)
-        browser_log.merge(params).merge(event.payload[:users])
-      end
-
-      initializer 'lograge-params.add_controller_hook' do
-        ActiveSupport.on_load :action_controller do
-          ActionController::Base.send(:include, LogrageParams::Controller)
+    config.lograge.custom_options = lambda do |event|
+      unwanted_keys = %w[format action controller
+                         utf8 authenticity_token commit]
+      flattener = proc do |(k, v), h, prefix='params'|
+        key = [prefix, k].compact.join("/")
+        if %w(password password_confirmation).include? k
+          h[key] = "[FILTERED]"
+        elsif v.is_a?(Hash)
+          v.each {|p| flattener.call(p, h, key)}
+        else
+          h[key] = "'#{v}'"
         end
       end
-    else
-      config.lograge.enabled = false
+
+      user_agent = UserAgent.parse(event.payload[:browser])
+      browser_log = {
+        browser: user_agent.browser,
+        platform: user_agent.platform,
+        browser_version: user_agent.version
+      }
+
+      params = event.payload[:params].reject { |key,_| unwanted_keys.include? key }.each_with_object({}, &flattener)
+      browser_log.merge(params).merge(event.payload[:users])
+    end
+
+
+    initializer 'lograge-params.add_controller_hook' do
+      ENV['USE_LOGRAGE'] ||= 'true' if Rails.env.production?
+
+      if ENV['USE_LOGRAGE'] == 'true'
+        config.lograge.enabled = true
+      else
+        config.lograge.enabled = false
+      end
+
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.send(:include, LogrageParams::Controller)
+      end
     end
   end
 end
