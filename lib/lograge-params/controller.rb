2d28dfd0d96ed8795dec1f081ff883e6e40f7e37
David Workman <workmad3@gmail.com>
2014-04-29 12:30:51 +0100

    Improving user logging

--- lib/lograge-params/controller.rb
+++ lib/lograge-params/controller.rb
@@ -2,11 +2,20 @@ module LogrageParams
   module Controller
     def append_info_to_payload(payload)
       super
+
       payload[:params] = request.params
-      if defined?(current_user) && current_user
-        payload[:user] = current_user.id
-      else
+      devise_users = methods.grep(/^current_/).each_with_object({}) do |m, hsh|
+        user_type = m.to_s.gsub(/^current_/, '')
+        u = send(m)
+        hsh[user_type] = u if u
+      end
+
+      if devise_users.empty?
         payload[:user] = "None"
+      else
+        devise_users.each do |type, user|
+          payload[type] = current_user.id
+        end
       end
     end
   end
