b5a672d8283c361fa1fe9be10b885dc404b3e520
David Workman <workmad3@gmail.com>
2014-04-29 12:51:15 +0100

    Fixing bug in logging

--- lib/lograge-params/controller.rb
+++ lib/lograge-params/controller.rb
@@ -7,7 +7,7 @@ module LogrageParams
       devise_users = methods.grep(/^current_/).each_with_object({}) do |m, hsh|
         user_type = m.to_s.gsub(/^current_/, '')
         u = send(m)
-        hsh[user_type] = u if u
+        hsh[user_type] = u if u && u.respond_to?(:id)
       end
       payload[:browser] = request.user_agent
 
