4de8c14239fd4dc6d4e60ddaa390bdcc426f8f2a
David Workman <workmad3@gmail.com>
2014-05-01 14:38:51 +0100

    Adding some extra handling

--- lib/lograge-params/controller.rb
+++ lib/lograge-params/controller.rb
@@ -4,21 +4,18 @@ module LogrageParams
       super
 
       payload[:params] = request.params
-      devise_users = methods.grep(/^current_/).each_with_object({}) do |m, hsh|
-        user_type = m.to_s.gsub(/^current_/, '')
-        u = send(m)
-        hsh[user_type] = u if u && u.respond_to?(:id)
-      end
-      payload[:browser] = request.user_agent
 
       payload[:users] = {}
-      if devise_users.empty?
-        payload[:users][:user] = "None"
-      else
-        devise_users.each do |type, user|
-          payload[:users][type] = user.id
-        end
+      methods.grep(/^current_/).each_with_object(payload[:users]) do |m, hsh|
+        user_type = m.to_s.gsub(/^current_/, '')
+        u = send(m)
+        hsh[user_type] = u.id if u && u.respond_to?(:id)
       end
+      if payload[:users].empty?
+        payload[:users][:user] = "None"
+      end
+
+      payload[:browser] = request.user_agent
     end
   end
 end
--- lib/lograge-params/engine.rb
+++ lib/lograge-params/engine.rb
@@ -6,28 +6,38 @@ ENV['USE_LOGRAGE'] ||= 'true'
 module LogrageParams
   class Engine < ::Rails::Engine
     config.lograge.custom_options = lambda do |event|
-      unwanted_keys = %w[format action controller
-                         utf8 authenticity_token commit]
-      flattener = proc do |(k, v), h, prefix='params'|
-        key = [prefix, k].compact.join("/")
-        if %w(password password_confirmation).include? k
-          h[key] = "[FILTERED]"
-        elsif v.is_a?(Hash)
-          v.each {|p| flattener.call(p, h, key)}
-        else
-          h[key] = "'#{v}'"
+      begin
+        unwanted_keys = %w[format action controller
+                           utf8 authenticity_token commit]
+        flattener = proc do |(k, v), h, prefix='params'|
+          key = [prefix, k].compact.join("/")
+          if %w(password password_confirmation).include? k
+            h[key] = "[FILTERED]"
+          elsif v.is_a?(Hash)
+            v.each {|p| flattener.call(p, h, key)}
+          else
+            h[key] = "'#{v}'"
+          end
         end
+
+        if event.payload[:browser]
+          user_agent = UserAgent.parse(event.payload[:browser])
+          browser_log = {
+            browser: user_agent.browser,
+            platform: user_agent.platform,
+            browser_version: user_agent.version
+          }
+        else
+          browser_log = {
+            browser: "Unknown"
+          }
+        end
+
+        params = event.payload[:params].reject { |key,_| unwanted_keys.include? key }.each_with_object({}, &flattener)
+        config.lograge.static_data.merge(browser_log).merge(params).merge(event.payload[:users])
+      rescue
+        {}
       end
-
-      user_agent = UserAgent.parse(event.payload[:browser])
-      browser_log = {
-        browser: user_agent.browser,
-        platform: user_agent.platform,
-        browser_version: user_agent.version
-      }
-
-      params = event.payload[:params].reject { |key,_| unwanted_keys.include? key }.each_with_object({}, &flattener)
-      config.lograge.static_data.merge(browser_log).merge(params).merge(event.payload[:users])
     end
 
     if ENV['USE_LOGRAGE'] == 'true'
