64d06f459193f42aab8f6ccd28c52ada14b29d7c
David Workman <workmad3@gmail.com>
2014-05-01 15:08:44 +0100

    Fixing issue in logging

--- lib/lograge-params/controller.rb
+++ lib/lograge-params/controller.rb
@@ -7,6 +7,8 @@ module LogrageParams
 
       payload[:users] = {}
       methods.grep(/^current_/).each_with_object(payload[:users]) do |m, hsh|
+        next if m == :current_inviter # This is badly implemented in devise_invitable. Skip it
+        next if m == :current_ability # This isn't anything we care about logging. It's from cancan
         user_type = m.to_s.gsub(/^current_/, '')
         u = send(m)
         hsh[user_type] = u.id if u && u.respond_to?(:id)
