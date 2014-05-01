f54733ce8abd34fa5df2cdabb7b816c34ab678aa
David Workman <workmad3@gmail.com>
2014-05-01 15:10:17 +0100

    Only add our custom stuff if lograge is enabled

--- lib/lograge-params/controller.rb
+++ lib/lograge-params/controller.rb
@@ -3,21 +3,23 @@ module LogrageParams
     def append_info_to_payload(payload)
       super
 
-      payload[:params] = request.params
+      if ENV["USE_LOGRAGE"]
+        payload[:params] = request.params
 
-      payload[:users] = {}
-      methods.grep(/^current_/).each_with_object(payload[:users]) do |m, hsh|
-        next if m == :current_inviter # This is badly implemented in devise_invitable. Skip it
-        next if m == :current_ability # This isn't anything we care about logging. It's from cancan
-        user_type = m.to_s.gsub(/^current_/, '')
-        u = send(m)
-        hsh[user_type] = u.id if u && u.respond_to?(:id)
-      end
-      if payload[:users].empty?
-        payload[:users][:user] = "None"
-      end
+        payload[:users] = {}
+        methods.grep(/^current_/).each_with_object(payload[:users]) do |m, hsh|
+          next if m == :current_inviter # This is badly implemented in devise_invitable. Skip it
+          next if m == :current_ability # This isn't anything we care about logging. It's from cancan
+          user_type = m.to_s.gsub(/^current_/, '')
+          u = send(m)
+          hsh[user_type] = u.id if u && u.respond_to?(:id)
+        end
+        if payload[:users].empty?
+          payload[:users][:user] = "None"
+        end
 
-      payload[:browser] = request.user_agent
+        payload[:browser] = request.user_agent
+      end
     end
   end
 end
